9e86eb035eab5ad8246c5d512b70fb3a54857483
Ryan Northey <ryan@3ca.org.uk>
2012-06-11 18:21:22 +0100

    macros

--- bit/bot/aiml/macros.py
+++ bit/bot/aiml/macros.py
@@ -14,22 +14,19 @@ from bit.bot.aiml.interfaces import IWho, IWhat, IWhere
 class WhoIs(BotAIMacro):
     implements(IAIMLMacro)
 
-    def parse(self, kernel, elem):
-        query = kernel._processElement(['star',{}], self.request)
-        return getUtility(IWho).search(self.request, query)
+    def parse(self, elem):
+        return getUtility(IWho).search(self.request, self.star)
 
 
 class WhatIs(BotAIMacro):
     implements(IAIMLMacro)
 
-    def parse(self, kernel, elem):
-        query = kernel._processElement(['star',{}], self.request)
-        return getUtility(IWhat).search(self.request, query)
+    def parse(self, elem):
+        return getUtility(IWhat).search(self.request, self.star)
 
 
 class WhereIs(BotAIMacro):
     implements(IAIMLMacro)
 
-    def parse(self, kernel, elem):
-        query = kernel._processElement(['star',{}], self.request)
-        return getUtility(IWhere).search(self.request, query)
+    def parse(self, elem):
+        return getUtility(IWhere).search(self.request, self.star)
